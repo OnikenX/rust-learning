be03ee98d821603c3889b3d914e87246f78f9a35
OnikenX <OnikenX@pm.me>
2021-04-07 18:20:46 +0100

    inicial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust learning
+
+
+## origem do libp2p-test
+https://blog.logrocket.com/libp2p-tutorial-build-a-peer-to-peer-app-in-rust/
---
+++ src/brincadeiras.rs
@@ -0,0 +1,28 @@
+
+fn cenas(){
+    println!("Hello boys!");
+    let i : i64 = 5;let mut num = 10;
+    print!("{}",i);
+    num+=1;
+    println!("Max i8 {}", i8::MAX);
+    let is_it_true = true;
+    println!("I am {1} years {0}", "15", "old");
+    println!("{:.2}", 1.234);
+
+    //tips of how to output
+    println!("B: {0:b} H: {0:x} O: {0:o}",10 );
+
+    //named arguments
+    println!("{var}", var = 2);
+
+    //white spaces
+    println!("{1:a<0$}", 5, 2);
+
+    for z in 1..10{
+        println!("FOR : {}", z);
+    }
+}
+
+fn fac (num : u32) -> u32 {
+    (1..=num).product()
+}
